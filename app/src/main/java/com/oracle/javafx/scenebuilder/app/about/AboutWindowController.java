3771a2d86cc0ee078894e2672ffe01ad2f87a5e8
georgemoralis <giorgosmrls@gmail.com>
2020-11-20 14:31:54 +0200

    fixing properties

--- app/src/main/java/com/oracle/javafx/scenebuilder/app/about/AboutWindowController.java
+++ app/src/main/java/com/oracle/javafx/scenebuilder/app/about/AboutWindowController.java
@@ -1,34 +1,8 @@
 /*
- * Copyright (c) 2016, 2017 Gluon and/or its affiliates.
- * Copyright (c) 2012, 2014, Oracle and/or its affiliates, 2015, Gluon.
- * All rights reserved. Use is subject to license terms.
- *
- * This file is available and licensed under the following license:
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *  - Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *  - Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the distribution.
- *  - Neither the name of Oracle Corporation and Gluon nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (c) 2020, codebb.gr and/or its affiliates.
+ * Copyright (c) 2015, 2016, 2017 Gluon and/or its affiliates.
+ * Copyright (c) 2012, 2014, Oracle and/or its affiliates
+ * Check license.txt for license
  */
 package com.oracle.javafx.scenebuilder.app.about;
 
@@ -39,6 +13,8 @@ import com.oracle.javafx.scenebuilder.kit.editor.panel.util.AbstractFxmlWindowCo
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
@@ -75,8 +51,9 @@ public final class AboutWindowController extends AbstractFxmlWindowController {
         sbProps.load(in);
         sbBuildInfo = sbProps.getProperty("build.info", "UNSET"); // NOI18N
         sbBuildVersion = sbProps.getProperty("build.version", "UNSET"); // NOI18N
-        sbBuildDate = sbProps.getProperty("build.date", "UNSET"); // NOI18N
-        sbBuildJavaVersion = sbProps.getProperty("build.java.version", "UNSET"); // NOI18N
+        sbBuildDate = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date());
+        sbBuildJavaVersion =
+            System.getProperty("java.runtime.version") + ", " + System.getProperty("java.vendor");
         sbAboutCopyrightKeyName = sbProps.getProperty("copyright.key.name", "UNSET"); // NOI18N
       }
     } catch (IOException ex) {
